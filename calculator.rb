61fe9d0c778bdc955209016681facabd98f1fc88
Loretta Marie Hughes <kchohee0630@gmail.com>
2019-10-26 18:27:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++
@@ -1,7 +0,0 @@
-./calculator.rb
-  contains a local variable called first_number that is assigned to a number (FAILED - 1)
- 
-Failures:
- 
-  1) ./calculator.rb contains a local variable called first_number that is assigned to a number
-     Failure/Error: expect(first_number).to be_an(Integer).or be_a(Float)
